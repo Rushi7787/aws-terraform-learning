5337f1ba1fcaddf9bcf869e87dcdc2cac1cba6c4
Rushi7787 <rushikeshmshelke@gmail.com>
2026-02-03 11:26:34 +0530

    Create mian.tf

---
+++ terraform/man.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+ region = "ap-south-1"
+}
+
+resource "aws_instance" "example" {
+ ami           = "ami-0d473344347276854"
+ instance_type = "t2.micro"
+
+ tags ={
+  Name ="Terraform-EC2"
+  }
+ }
+
+- AWS Provider
+- Tags usage
