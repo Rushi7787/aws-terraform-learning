9046f78c903a0d5a61f872957ca89750a980e174
Rushi7787 <rushikeshmshelke@gmail.com>
2026-02-03 11:35:27 +0530

    Update man.tf

--- terraform/man.tf
+++ terraform/man.tf
@@ -1,15 +1,8 @@
 provider "aws" {
- region = "ap-south-1"
+ region = var.region
 }
 
 resource "aws_instance" "example" {
  ami           = "ami-0d473344347276854"
- instance_type = "t2.micro"
-
- tags ={
-  Name ="Terraform-EC2"
-  }
- }
-
- - AWS Provider
- - Tags usage
+ instance_type = var.instance_type
+}
